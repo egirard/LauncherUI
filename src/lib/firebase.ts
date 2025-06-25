ec4f60ed7483b0bd6cf50e6749ccfde32e7099f1
egirard <eugene.girard@gmail.com>
2025-06-25 13:05:18 -0400

    Formatting changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
   storageBucket: "launcherui.firebasestorage.app",
   messagingSenderId: "952373165690",
   appId: "1:952373165690:web:e46badad1e18e85f1285c3",
-  measurementId: "G-58QT3T3ZYQ"
+  measurementId: "G-58QT3T3ZYQ",
 };
 
 // Initialize Firebase
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
-    import { GoogleAuthProvider, getAuth } from "@firebase/auth";
-    import { initializeApp } from "@firebase/app";
-    import { Signin } from "@ourway/svelte-firebase-auth";
-    import { app, firestore } from "$lib/firebase";
-    import { doc, setDoc } from "@firebase/firestore";
-    import type { SignedInUser } from "@ourway/svelte-firebase-auth";
+import { app, firestore } from "$lib/firebase";
+import { initializeApp } from "@firebase/app";
+import { GoogleAuthProvider, getAuth } from "@firebase/auth";
+import { doc, setDoc } from "@firebase/firestore";
+import { Signin } from "@ourway/svelte-firebase-auth";
+import type { SignedInUser } from "@ourway/svelte-firebase-auth";
 
-    const auth = getAuth(app);
-    // console.log("AUTH: ", auth)
-    const googleAuthProvider = new GoogleAuthProvider();
+const auth = getAuth(app);
+// console.log("AUTH: ", auth)
+const googleAuthProvider = new GoogleAuthProvider();
 
-    let uid: string | undefined = undefined;
-    let userId: SignedInUser | undefined = undefined;
+let uid: string | undefined = undefined;
+let userId: SignedInUser | undefined = undefined;
 
-    function user(e: CustomEvent) {
-        // console.log("CUSTOM EVENT: ",e);
-        if (e.detail.signedIn) {
-            uid = e.detail.uid;
-            const userRecord = doc(firestore, `/users/${uid}`);
-            setDoc(userRecord, e.detail);
-            console.log("User signed in: ", e.detail);
-            userId = e.detail;
-        } else {
-            uid = undefined;
-        }
-    }
+function user(e: CustomEvent) {
+  // console.log("CUSTOM EVENT: ",e);
+  if (e.detail.signedIn) {
+    uid = e.detail.uid;
+    const userRecord = doc(firestore, `/users/${uid}`);
+    setDoc(userRecord, e.detail);
+    console.log("User signed in: ", e.detail);
+    userId = e.detail;
+  } else {
+    uid = undefined;
+  }
+}
 </script>
 
 <h1>Welcome to Tabletop's Launcher UI</h1>
