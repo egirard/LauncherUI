4018326bea148ce0b94cd6b4fa6b96ea9e674812
Alex Nicolaou <anicolao@gmail.com>
2026-08-01 09:45:54 -0400

    Add production Firebase PR previews

--- flake.nix
+++ flake.nix
@@ -21,8 +21,10 @@
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            nodejs
+            nodejs_22
             bun
+            actionlint
+            gh
             openjdk
           ];
         };
